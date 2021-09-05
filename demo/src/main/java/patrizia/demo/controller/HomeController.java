0c52242be0782c861853c480fea72746eeabd003
PatriziaGessa <patriziagessa@gmail.com>
2021-09-05 21:44:36 +0200

    Update

--- demo/src/main/java/patrizia/demo/controller/HomeController.java
+++ demo/src/main/java/patrizia/demo/controller/HomeController.java
@@ -1,13 +1,23 @@
 package patrizia.demo.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HomeController {
 
+    private boolean pageWasAccessed;
+
     @RequestMapping("/simplehome")
-    public String getHome() {
+    public String getHome(Model model) {
+        if (pageWasAccessed) {
+            model.addAttribute("firstVisit", false);
+
+        } else {
+            pageWasAccessed = true;
+            model.addAttribute("firstVisit", true);
+        }
         return "home";
     }
 }
